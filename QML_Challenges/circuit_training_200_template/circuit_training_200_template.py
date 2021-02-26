a5bd12ffcacbb47a43e82c484148d0c469fad520
Erfan Abedi <erfanabedi.78@gmail.com>
2021-02-26 19:40:14 +0330

    Added B.1, C.1 and C.2

--- QML_Challenges/circuit_training_200_template/circuit_training_200_template.py
+++ QML_Challenges/circuit_training_200_template/circuit_training_200_template.py
@@ -4,6 +4,7 @@ import sys
 import networkx as nx
 import numpy as np
 import pennylane as qml
+from matplotlib import pyplot as plt
 
 
 # DO NOT MODIFY any of these parameters
@@ -31,7 +32,59 @@ def find_max_independent_set(graph, params):
     max_ind_set = []
 
     # QHACK #
+    qaoa = qml.qaoa
+    cost_h, mixer_h = qaoa.max_independent_set(graph, constrained=True)
 
+    def qaoa_layer(gamma, alpha):
+        qaoa.cost_layer(gamma, cost_h)
+        qaoa.mixer_layer(alpha, mixer_h)
+
+    wires = range(NODES)
+
+    def circuit(params, **kwargs):
+        # for w in wires:
+            # qml.Hadamard(wires=w)
+        qml.layer(qaoa_layer, N_LAYERS, params[0], params[1])
+    
+    dev = qml.device("default.qubit", wires=wires)
+    # dev = qml.device("qulacs.simulator", wires=wires)
+    cost_function = qml.ExpvalCost(circuit, cost_h, dev)
+
+    optimizer = qml.GradientDescentOptimizer()
+    steps = 3
+    # params = [[0.5 for _ in range(N_LAYERS)] for _ in range(2)]
+
+    for _ in range(steps):
+        params = optimizer.step(cost_function, params)
+    
+    @qml.qnode(dev)
+    def probability_circuit(gamma, alpha):
+        circuit([gamma, alpha])
+        return qml.probs(wires=wires)
+
+    probs = probability_circuit(params[0], params[1])
+    probs_list = []
+    for p in probs:
+        probs_list.append(float(p))
+    # print(probs_list)
+    # probs[0] = 0.0
+
+    # print(probs)
+    # plt.style.use("seaborn")   
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(111)
+    # ax1.set_xticks(np.arange(len(probs)))
+    # ax1.bar(range(2 ** len(wires)), probs)
+    # plt.show()
+
+    max_prob = max(probs_list)
+    max_index = probs_list.index(max_prob)
+    bit_str = "{0:b}".format(max_index).zfill(NODES)
+    # print(bit_str)
+
+    for idx, val in enumerate(bit_str):
+        if val == "1":
+            max_ind_set.append(idx)
     # QHACK #
 
     return max_ind_set
